2cd36e28ece90eb18816d8825e72eb256775f51e
sinly2 <15380794704@163.com>
2018-08-24 01:41:34 +0800

    增加article

--- app/handlers/article.py
+++ app/handlers/article.py
@@ -5,6 +5,7 @@ Created on Aug 23, 2018
 @author: guxiwen
 """
 from flask import Blueprint, request
+from ..models.article import Article
 
 __all__ = ['bp']
 
@@ -13,4 +14,4 @@ bp = Blueprint('article', __name__)
 
 @bp.route('/article/<int:article_id>')
 def article(article_id=1):
-    pass
+    article = Article.query.filter(Article.id == article_id)
--- app/models/_base.py
+++ app/models/_base.py
@@ -5,6 +5,7 @@ Created on Aug 22, 2018
 @author: guxiwen
 """
 from app import db
+from flask_sqlalchemy import BaseQuery
 
 __all__ = ['SessionMixin']
 
@@ -14,3 +15,9 @@ class SessionMixin(object):
         db.session.add(self)
         db.session.commit()
         return self
+
+
+class YmmQuery(BaseQuery):
+    def filter_in(self):
+        pass
+db.Model.query_class = YmmQuery
